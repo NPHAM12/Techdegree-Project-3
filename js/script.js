b692c487f8472c54183c9664ebb44cdfb0f01bc8
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-01-30 15:13:12 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Build an Interactive Form
+Exceeds Expectations
+Nguyen Pham
+******************************************/
 /*Set focus on the first text field*/
 //When the page first loads, the first text field should be in focus by default.
 $(document).ready(function() {
   $('#name').focus();
 
   /* OTHER JOB ROLE SELECTION */
-  const $other = $('<input type="text" id="other" placeholder="Your job role">');
-  $('.basicInfo').append($other);
-  $('#other').hide();
+  // Hide Other Role describle section
+  $('#otherRole').hide();
   $('#title').on('change', function(e) {
-    if ($(target).val() === 'other')
-      $('#other').show();
+    if ($('#title option:selected').val() === 'other')
+      $('#otherRole').show();
     else
-      $('#other').hide();
+      $('#otherRole').hide();
   });
 
   /* "T-SHIRT" SELECTION*/
   // hide Select Theme option
   $('#design option[value="js puns"]').prev().hide();
-  $('#color').parent().hide(); //hide color dropdown initially
+  // $('#design option[value="js puns"]').prop("selected",true);
+  //hide color dropdown initially
+  $('#color').parent().hide();
   $('#design').change(function(e) {
     $('#color').parent().show(); // show color dropdown option when selecting design option
     $('#color').children('option').hide(); // hide all options
-    if ($(this).val() === 'js puns') {
+    // if ($(this).val() === 'js puns') {
+    if ($('#design option[value="js puns"]').prop("selected") === true) {
       $('#color').val('cornflowerblue'); // set a drop down box value to Cornflower value
       $('#color').children('option[value="cornflowerblue"]').show();
       $('#color').children('option[value="darkslategrey"]').show();
@@ -74,7 +82,6 @@ $(document).ready(function() {
         cost -= 100;
       }
     }
-    
     //Express Workshop
     if (target === $('input[name="express"]').get(0)) {
       if ($('input[name="express"]:checked').length !== 0) {
@@ -87,7 +94,6 @@ $(document).ready(function() {
         cost -= 100;
       }
     }
-    
     // Node.js Workshop
     if (target === $('input[name="node"]').get(0)) {
 
@@ -101,7 +107,6 @@ $(document).ready(function() {
         cost -= 100;
       }
     }
-    
     // Build tools Workshop
     if (target === $('input[name="build-tools"]').get(0)) {
       if ($('input[name="build-tools"]:checked').length !== 0) {
@@ -110,7 +115,6 @@ $(document).ready(function() {
         cost -= 100;
       }
     }
-    
     // npm Workshop
     if (target === $('input[name="npm"]').get(0)) {
       if ($('input[name="npm"]:checked').length !== 0) {
@@ -138,14 +142,12 @@ $(document).ready(function() {
       $('div p:contains("PayPal")').hide();
       $('div p:contains("Bitcoin")').hide();
     }
-    
     //Paypal Option
     else if ($('#payment option:selected').val() === "paypal") {
       $('#credit-card').hide();
       $('div p:contains("Bitcoin")').hide();
       $('div p:contains("PayPal")').show();
     }
-    
     //Bitcoin Option
     else {
       $('#credit-card').hide();
@@ -157,24 +159,40 @@ $(document).ready(function() {
 
   /*FORM VALIDATION*/
   //Error indicator
+  function addNote(element, text) {
+    let p = $('<span class="error-message"></span>');
+    p.text(text);
+    element.after(p);
+  }
+
+  //Error indicator
   function indicatorErr(element) {
-    element.css({'border':'3px solid red'});
-    element.prev().css({'color': 'red'});
+    element.css({
+      'border': '3px solid red'
+    });
+    element.prev().css({
+      'color': 'red'
+    });
     //special case: remove border at Main Conference checkbox
     if (element = $('.activities input[name="all"]').parent()) {
-      element = element.css({'border':''});
+      element = element.css({
+        'border': ''
+      });
     }
   }
 
   // reset Error indicator
   function resetErr(element) {
-    element.css({'border':''});
-    element.prev().css({'color':''});
+    element.css({
+      'border': ''
+    });
+    element.prev().css({
+      'color': ''
+    });
   }
 
-  //Check Name
+  //Validate Name
   function checkName() {
-    // alert('name');
     const pattern = /^[a-z\s]+$/i;
     const name = pattern.test($('#name').val()); //return true or false
     if (!name) {
@@ -185,9 +203,8 @@ $(document).ready(function() {
     return name;
   }
 
-  // Check Email
+  // Validate Email
   function checkEmail() {
-    // alert("email");
     const pattern = /^[^@]+@[^@.]+\.[a-z]+$/i;
     const email = pattern.test($('#mail').val()); //return true or false
     if (!email) { // no correct input value
@@ -197,8 +214,7 @@ $(document).ready(function() {
     resetErr($('#mail'));
     return email;
   }
-  
-  // //Check Register for Activities
+  //Validate Register for Activities
   function checkRegister() {
     if ($('.activities input[type="checkbox"]:checked').length === 0) {
       indicatorErr($('.activities input[name="all"]').parent());
@@ -208,12 +224,11 @@ $(document).ready(function() {
     return true;
   }
 
-  //check Card number
+  //Validate Card number
   function checkCardNumber() {
     const patternCardNumber = /^[\d]{13,16}$/;
     const cardNumber = patternCardNumber.test($('#cc-num').val());
     if (!cardNumber) {
-      // alert("number");
       indicatorErr($('#cc-num'));
       return cardNumber;
     }
@@ -221,12 +236,11 @@ $(document).ready(function() {
     return cardNumber;
   }
 
-  //check Zipcode
+  //Validate Zipcode
   function checkZipcode() {
     const patternZipCode = /^[\d]{5}$/;
     const zipCode = patternZipCode.test($('#zip').val());
     if (!zipCode) {
-      // alert("zipcode");
       indicatorErr($('#zip'));
       return zipCode;
     }
@@ -234,12 +248,11 @@ $(document).ready(function() {
     return zipCode;
   }
 
-  //check CVV
+  //Validate CVV
   function checkCvv() {
     const patternCvv = /^[\d]{3}$/;
     const cvv = patternCvv.test($('#cvv').val());
     if (!cvv) {
-      // alert("cvv");
       indicatorErr($('#cvv'));
       return cvv;
     }
@@ -249,35 +262,54 @@ $(document).ready(function() {
 
   /*Submit Form*/
   $('button').on('click', function(e) {
-    alert("1");
-    //Check all at first
-    checkName();
-    checkEmail();
-    checkRegister();
-    checkCardNumber();
-    checkZipcode();
-    checkCvv();
-    //Check if payment method is credit card
-    if ($('#payment option:selected').val() === "credit card") {
-      if (!checkCardNumber()) {
-        e.preventDefault();
-      }
-      if (!checkZipcode()) {
-        e.preventDefault();
-      }
-      if (!checkCvv()) {
-        e.preventDefault();
-      }
-    }
-    // Preven submit form if one of them failed
-    if (!checkName()
-        || !checkEmail()
-        || !checkRegister()
-        || !checkCardNumber()
-        || !checkZipcode()
-        || !checkCvv()) {
-      alert("here");
+    // Clear error message
+    $("span.error-message").remove();
+    //Check all requirements at first
+    if (!checkName()) {
       e.preventDefault();
+      addNote($('#name'), "Name should NOT be a number or empty!");
     }
+    if (!checkEmail()) {
+      e.preventDefault()
+      addNote($('#mail'), "Email should NOT be empty or have 2 more than @!");
+    };
+    if (!checkRegister()) {
+      e.preventDefault();
+      addNote($('.activities'), "Do NOT forget to select at leat 1 option!");
+    };
+    if (!checkCardNumber()) {
+      addNote($('#cc-num'), "Number should be 13 - 16 digits!");
+    }
+    if (!checkZipcode()) {
+      addNote($('#zip'), "Number should be 5 digits!");
+    }
+    if (!checkCvv()) {
+      addNote($('#cvv'), "Number should be 3 digits!");
+    }
+
+    //Validate payment method
+    if ($('#payment option:selected').val() !== "credit card") {
+      if ($('#payment option:selected').val() !== "paypal") {
+        if ($('#payment option:selected').val() !== "bitcoin") {
+          addNote($('#payment'), "Please, select a payment");
+          e.preventDefault();
+        }
+      }
+    } else {
+      // Validate card information if payment method is credit card
+      if ($('#payment option:selected').val() === "credit card") {
+        if (!checkCardNumber()) {
+          e.preventDefault();
+        }
+        if (!checkZipcode()) {
+          e.preventDefault();
+        }
+        if (!checkCvv()) {
+          e.preventDefault();
+        }
+      }
+    }
+    if (!confirm('Are you sure that you want to submit the form'))
+      e.preventDefault();
   });
 });
